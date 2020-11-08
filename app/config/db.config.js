aea837c5485a3af733e18230b0fee0ac17a2aaf6
adidas3559 <forbes3559@gmail.com>
2020-11-08 15:32:40 -0600

    trying with cleardb

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,11 +1,26 @@
+// module.exports = {
+//   //This is my localhost, ask if it can be other localhosts
+//     // HOST: "localhost",
+//     HOST: "192.168.1.13",
+//     USER: "forbes",
+//     PASSWORD: "@SMN3559indians!",
+//     DB: "rugby",
+//     dialect: "mysql",
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000
+//     }
+//   };
+
 module.exports = {
   //This is my localhost, ask if it can be other localhosts
     // HOST: "localhost",
-    HOST: "192.168.1.13",
-    PORT: "3306",
-    USER: "forbes",
-    PASSWORD: "@SMN3559indians!",
-    DB: "rugby",
+    HOST: "us-cdbr-east-02.cleardb.com",
+    USER: "b752f769cc730d",
+    PASSWORD: "c3778a81",
+    DB: "heroku_e4d52b6b68ade3b",
     dialect: "mysql",
     pool: {
       max: 5,
